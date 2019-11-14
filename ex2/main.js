f28e90a6095759588385725c62b20ad829cbe970
Nikos Vasilakis <nikos@vasilak.is>
2019-11-14 07:56:46 -0500

    New exercise

---
+++ ex2/main.js
@@ -0,0 +1,9 @@
+
+let m = require('math.js');
+
+// let txfm = require('./txfm.js');
+// m = txfm.wrap(m);
+
+
+m.x = 3;
+m.fft.mul(m.add(m.constants.pi, m.constants.e), m.x);
---
+++ ex2/math.js
@@ -0,0 +1,14 @@
+// a simple math library
+module.exports = {
+  add: (a, b) => a + b,
+  sub: (a, b) => a - b
+  constants: {
+    pi: 3.14,
+    e: 2.71
+  }
+  fft: {
+    add: (a, b) => a + b,
+    sub: (a, b) => a - b
+  }
+}
+    
