97d7b5bd9e1632fed63bdce04a8a433f0334de84
GNtousakis <greco1996@gmail.com>
2019-11-16 13:25:48 +0200

    ex3

---
+++ ex3/main.js
@@ -0,0 +1,7 @@
+require = require('./require_counter.js');
+
+//Testing that the counter increases
+let m = require('./math.js');
+let mod = require('module');
+let fs = require('fs');
+
---
+++ ex3/require_counter.js
@@ -0,0 +1,25 @@
+//We import the module that stores the require function 
+let Module = require('module');
+const assert = require('assert');
+
+//We declare the counter of require
+let counter = 0;
+
+//We just increace a counter and then call the original require
+Module.prototype.require = (name) => {
+	counter++;
+	console.log("Require has been called", counter);
+
+	//Original functionality of require
+	assert(name, 'missing path');
+  	assert(typeof name === 'string', 'path must be a string');
+  	return Module._load(name, this);
+};
+
+
+module.exports = Module.prototype.require;
+
+
+
+
+
