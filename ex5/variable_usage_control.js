67c52fd1e7ba13993b08cd43bedf85464f2ec2ca
GNtousakis <greco1996@gmail.com>
2019-11-28 15:00:49 +0200

    updated variable_usage_control

--- ex5/variable_usage_control.js
+++ ex5/variable_usage_control.js
@@ -20,41 +20,40 @@ let count = 0;
 //Handlers of Proxies
 //The handler of the functions
 let handler= {
-	apply: function (target) {
-		let curr_name = true_name[count];
-		if (variable_call[curr_name].hasOwnProperty(target.name) === false)	
-			variable_call[curr_name][target.name] = true;
+  apply: function (target) {
+    let curr_name = true_name[count];
+    if (variable_call[curr_name].hasOwnProperty(target.name) === false) 
+      variable_call[curr_name][target.name] = true;
 
-		return Reflect.apply( ...arguments);	
-	},
-	get: function(target, name){
-		let curr_name = true_name[count];
-		if (variable_call[curr_name].hasOwnProperty(target.name) === false)	
-			variable_call[curr_name][target.name] = true;
+    return Reflect.apply( ...arguments);  
+  },
+  get: function(target, name){
+    let curr_name = true_name[count];
+    if (variable_call[curr_name].hasOwnProperty(target.name) === false) 
+      variable_call[curr_name][target.name] = true;
 
     return Reflect.get(target, name);
-	}
+  }
 }
 
 //The handler of the imported libraries
 let handler_exports= {
-	apply: function (target) {
+  apply: function (target) {
     let curr_name = arguments[1].truepath;
-	let	truename = arguments[1].truename;
+  let truename = arguments[1].truename;
     truename = truename + "." + target.name;
-		if (variable_call[curr_name].hasOwnProperty(truename) === false)	
-			variable_call[curr_name][truename] = true;
+    if (variable_call[curr_name].hasOwnProperty(truename) === false)  
+      variable_call[curr_name][truename] = true;
  
-		return Reflect.apply( ...arguments);
-	}
+    return Reflect.apply( ...arguments);
+  }
 }
 
 //The handler of require --we need to import the name
 let handler_require= {
   apply: function (target) {
     let curr_name = true_name[count];
-    let name_req = target.name + '(\"'+ arguments[2][0] + "\")";  //In arguments[2][0] is the name we use to import
-    if (variable_call[curr_name].hasOwnProperty(name_req) === false) 
+     let name_req = target.name + '(\''+ arguments[2][0] + "\')";  //In arguments[2][0] is the name we use to import    if (variable_call[curr_name].hasOwnProperty(name_req) === false) 
       variable_call[curr_name][name_req] = true;
 
     return Reflect.apply( ...arguments);  
@@ -65,36 +64,53 @@ let handler_require= {
 //We wrap the compiledWrapper code in a proxy so
 //when it is called it will do this actions =>
 let handler_addArg= {
-	apply: function (target) {
-		let local_require = arguments[2][1];	//We catch local require in order to wrap it
-		local_require = new Proxy(local_require, handler_require);
-		arguments[2][1] = local_require;	//We wrap require
-		arguments[2][5] = global_proxy;		//We pass the global values with the proxies
-		let result = Reflect.apply( ...arguments);
+  apply: function (target) {
+    let local_require = arguments[2][1];  //We catch local require in order to wrap it
+    local_require = new Proxy(local_require, handler_require);
+    arguments[2][1] = local_require;  //We wrap require
+    arguments[2][5] = global_proxy;   //We pass the global values with the proxies
+    let result = Reflect.apply( ...arguments);
 
-		return result;
-	}	
+    return result;
+  } 
+}
+
+let handler_obj_export= {
+  get: function(target, name){
+    if(typeof target[name] != 'string' && typeof target[name] != 'undefined'){
+      if (typeof target[name] === 'object') {
+        let local_object = target[name];
+        target[name] = new Proxy (local_object, handler_obj_export);
+        target[name].truename = target['truename'] + '.' + name ;
+        target[name].truepath = target['truepath'];
+      }else{
+        let local_function = target[name];
+        target[name] = new Proxy(local_function, handler_exports)
+      }
+    }
+    return Reflect.get(target, name);
+  }
 }
 
 //Returns the proxy obj we want
 let proxy_wrap = function(handler,obj) {
-	if (typeof obj === 'function') {
-		obj = new Proxy(obj, handler);
-		return obj;
-	}
+  if (typeof obj === 'function') {
+    obj = new Proxy(obj, handler);
+    return obj;
+  }
 
-  	for (k in obj) {
-  		let type = typeof obj[k];
-    	if (type === 'number' ||  type === 'boolean' ) {
-      		obj[k] = obj[k];  
-    	}else if (type === 'object') {
-    	  	obj[k] = proxy_wrap(obj[k]);
-   	 	}else{
-   	 		obj[k] = new Proxy(obj[k], handler);
-    	}
-  	}
+    for (k in obj) {
+      let type = typeof obj[k];
+      if (type === 'number' ||  type === 'boolean' ) {
+          obj[k] = obj[k];  
+      }else if (type === 'object') {
+          obj[k] = proxy_wrap(obj[k]);
+      }else{
+        obj[k] = new Proxy(obj[k], handler);
+      }
+    }
 
-	return obj;
+  return obj;
 } 
 
 //https://stackoverflow.com/questions/14962018/detecting-and-fixing-circular-references-in-javascript
@@ -144,19 +160,19 @@ function isCyclic(object) {
 let proxy_wrap_imports = function(obj, handler, path) {
   for (k in obj) {
     if (typeof obj[k] === 'number') {
-      	obj[k] = obj[k];  //no action 
+        obj[k] = obj[k];  //no action 
     }else if (typeof obj[k] === 'object') {
-    	if (isCyclic(obj[k])){ //Fixes the circular references
-    		obj[k] = obj[k];  //no action
-    	} else{
-    		obj[k].truename = 'require(\"' + path + '\")';
-    		obj[k].truepath = true_name[count];
-      		obj[k] = proxy_wrap_imports(obj[k], handler);
-    	}
+      if (isCyclic(obj[k])){ //Fixes the circular references
+        obj[k] = obj[k];  //no action
+      } else{
+        obj[k].truename = 'require(\'' + path + '\')';
+        obj[k].truepath = true_name[count];
+        obj[k] = proxy_wrap_imports(obj[k], handler);
+      }
     }else{
-    	try{
-    		obj[k] = new Proxy(obj[k], handler);
-    	}catch(TypeError){}
+      try{
+        obj[k] = new Proxy(obj[k], handler);
+      }catch(TypeError){}
     }
   } 
 
@@ -169,40 +185,40 @@ let json_data = JSON.parse(rawdata);
 
 //We need to add all the global variable declarations in the script 
 let globals_decl = () => {
-	let final_decl = ' ';
-	for (let up_value in json_data) {
-		let global_variables = json_data[up_value];
-		for (let decl_name in global_variables){
-			let name = global_variables[decl_name];
-			final_decl = create_global(name, final_decl);
-		}		
-	}
+  let final_decl = ' ';
+  for (let up_value in json_data) {
+    let global_variables = json_data[up_value];
+    for (let decl_name in global_variables){
+      let name = global_variables[decl_name];
+      final_decl = create_global(name, final_decl);
+    }   
+  }
 
-	return final_decl;
+  return final_decl;
 }
 
 //We declare the data on the same time to pass them inside wrapped function
 let create_global = (name, final_decl) => {
-	if (global[name] != undefined){
-		global_proxy[name] = proxy_wrap(handler, global[name]);
-		final_decl = 'let ' + name + ' = pr.' + name +';\n' + final_decl; 
-	}
+  if (global[name] != undefined){
+    global_proxy[name] = proxy_wrap(handler, global[name]);
+    final_decl = 'let ' + name + ' = pr.' + name +';\n' + final_decl; 
+  }
 
-	return final_decl;
+  return final_decl;
 }
 
 //We do some stuff and then call original warp
 Module.wrap = (script) => {
-	script = globals_decl() +  script;
-	let wrapped_script = original_warp(script);
-	wrapped_script = wrapped_script.replace('__dirname)', '__dirname, pr)');
-	return wrapped_script;
+  script = globals_decl() +  script;
+  let wrapped_script = original_warp(script);
+  wrapped_script = wrapped_script.replace('__dirname)', '__dirname, pr)');
+  return wrapped_script;
 }
 
 //Returns the last location of a path
 let get_name = (way_file) => {
-	let splited = way_file.split('/')
-	return splited[splited.length - 1];
+  let splited = way_file.split('/')
+  return splited[splited.length - 1];
 };
 
 //We export the name of the curr module and pass proxy to the final function
@@ -216,22 +232,27 @@ vm.runInThisContext = function(code, options) {
 
 //We wrap the result in the wrapper function
 Module.prototype.require = (path) => {
-	let result = original_require(path,this);
-	if(result.truename === undefined ){
-		result = proxy_wrap_imports(result, handler_exports, path);
-    result.truename = 'require(\"' + path + '\")';
+  let result = original_require(path,this);
+  if(result.truename === undefined ){
+    //result = proxy_wrap_imports(result, handler_exports, path);
+    result = new Proxy (result,handler_obj_export);
+    result.truename = 'require(\'' + path + '\')';
     result.truepath = true_name[count];
-		if (count !=1) count--;	 
-	}else{
-    result.truename = 'require(\"' + path + '\")';
+    if (count !=1) count--;  
+  }else{
+    result.truename = 'require(\'' + path + '\')';
     result.truepath = true_name[count];
   }
-
-	return result;
+  return result;
 }
 
 //We print all the results on the end of the program
 process.on('exit', function() {
-    console.log("---------------------");
-    console.log(variable_call);
+    console.log(variable_call)
+    console.log(JSON.stringify(variable_call));
 });
+
+// require.cache[path.resolve(__dirname)]
+// module.exports = 
+// module.parent
+// ;
