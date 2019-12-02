2ac71be9bf340a52f5ea5b1719a7afcab0d6c740
GNtousakis <greco1996@gmail.com>
2019-12-02 10:26:25 +0200

    fixed most cosmetic errors --need to fix prefer-rest-params & max-len

--- src/txfm.js
+++ src/txfm.js
@@ -1,211 +1,220 @@
-//We import and declare all the necessary modules  
+// We import and declare all the necessary modules
 const Module = require('module');
 const vm = require('vm');
 const fs = require('fs');
 
-//All the necessary modules for swap
-let original_warp = Module.wrap;
-let original_require = Module.prototype.require;
-let original_resolveFilename = Module._resolveFilename;
-let original_run = vm.runInThisContext;
+// All the necessary modules for swap
+const originalWrap = Module.wrap;
+const originalRequire = Module.prototype.require;
+const originalRun = vm.runInThisContext;
 
-//We declare the variables
-let global_proxy = {};
-let variable_call = {};
+// We declare the variables
+const globalProxy = {};
+const variableCall = {};
 
-//We store names as a lifo
-let true_name = {};
+// We store names as a lifo
+const trueName = {};
 let count = 0;
 
-//Handlers of Proxies
-//The handler of the functions
-let handler= {
-  apply: function (target) {
-    let curr_name = true_name[count];
-    if (variable_call[curr_name].hasOwnProperty(target.name) === false) 
-      variable_call[curr_name][target.name] = true;
+// Handlers of Proxies
+// The handler of the functions
+const handler= {
+  apply: function(target) {
+    const currentName = trueName[count];
+    if (Object.prototype.hasOwnProperty.call(variableCall[currentName], target.name) === false) {
+      variableCall[currentName][target.name] = true;
+    }
 
-    return Reflect.apply( ...arguments);  
+    return Reflect.apply(...arguments);
   },
-  get: function(target, name){
-    let curr_name = true_name[count];
-    if (variable_call[curr_name].hasOwnProperty(target.name) === false) 
-      variable_call[curr_name][target.name] = true;
+  get: function(target, name) {
+    const currentName = trueName[count];
+    if (Object.prototype.hasOwnProperty.call(variableCall[currentName], target.name) === false) {
+      variableCall[currentName][target.name] = true;
+    }
 
     return Reflect.get(target, name);
-  }
-}
+  },
+};
 
-//The handler of the imported libraries
-let handler_exports= {
-  apply: function (target) {
-    let curr_name = arguments[1].truepath;
+// The handler of the imported libraries
+const handlerExports= {
+  apply: function(target) {
+    const currentName = arguments[1].truepath;
     let truename = arguments[1].truename;
-    truename = truename + "." + target.name;
-    if (variable_call[curr_name].hasOwnProperty(truename) === false)  
-      variable_call[curr_name][truename] = true;
+    truename = truename + '.' + target.name;
+    if (Object.prototype.hasOwnProperty.call(variableCall[currentName], truename) === false) {
+      variableCall[currentName][truename] = true;
+    }
 
     return Reflect.apply( ...arguments);
-  }
-}
+  },
+};
 
-//The handler of require --we need to import the name
-let handler_require= {
-  apply: function (target) {
-    let curr_name = true_name[count];
-    let name_req = target.name + '(\''+ arguments[2][0] + "\')";  //In arguments[2][0] is the name we use to import    if (variable_call[curr_name].hasOwnProperty(name_req) === false) 
-    variable_call[curr_name][name_req] = true;
-    return Reflect.apply( ...arguments);  
-  }
-}
+// The handler of require --we need to import the name
+const handlerRequire= {
+  apply: function(target) {
+    const currentName = trueName[count];
+    const nameReq = target.name + '(\''+ arguments[2][0] + '\')';// In arguments[2][0] is the name we use to import    if (variableCall[currentName].hasOwnProperty(name_req) === false)
+    variableCall[currentName][nameReq] = true;
 
-//The handler of compiledWrapper
-//We wrap the compiledWrapper code in a proxy so
-//when it is called it will do this actions =>
-let handler_addArg= {
-  apply: function (target) {
-    let local_require = arguments[2][1];  //We catch local require in order to wrap it
-    local_require = new Proxy(local_require, handler_require);
-    arguments[2][1] = local_require;  //We wrap require
-    arguments[2][5] = global_proxy;   //We pass the global values with the proxies
-    let result = Reflect.apply( ...arguments);
+    return Reflect.apply( ...arguments);
+  },
+};
+
+// The handler of compiledWrapper
+// We wrap the compiledWrapper code in a proxy so
+// when it is called it will do this actions =>
+const handlerAddArg= {
+  apply: function(target) {
+    let localRequire = arguments[2][1];// We catch local require in order to wrap it
+    localRequire = new Proxy(localRequire, handlerRequire);
+    arguments[2][1] = localRequire;// We wrap require
+    arguments[2][5] = globalProxy;// We pass the global values with the proxies
+    const result = Reflect.apply( ...arguments);
 
     return result;
-  } 
-}
+  },
+};
 
-//We first wrap the export obj so that we avoid to print functions that are not called by us
-let handler_obj_export= {
-  get: function(target, name){
-    if(typeof target[name] != 'undefined'){
-      if (typeof target[name] === 'object') { //if we try to grab an object we wrap it in this proxy
-        let local_object = target[name];
-        target[name] = new Proxy (local_object, handler_obj_export);
-        target[name].truename = target['truename'] + '.' + name ; //and update truename
+// We first wrap the export obj so that we avoid to print functions that are not called by us
+const handlerObjExport= {
+  get: function(target, name) {
+    if (typeof target[name] != 'undefined') {
+      if (typeof target[name] === 'object') {// If we try to grab an object we wrap it in this proxy
+        const localObject = target[name];
+        target[name] = new Proxy(localObject, handlerObjExport);
+        target[name].truename = target['truename'] + '.' + name;// And update truename
         target[name].truepath = target['truepath'];
-      }else if (typeof target[name] === 'string' ) {  //if we try to call a string that 
-        if (name != 'truename' && name != 'truepath') { //is not truename or truepath
-          let truename = target.truename;       //we need to print access to that variable
-          let truepath = true_name[count];    //we take the path that we are by using true_count
-          truename = truename + "." + name;
-          if (variable_call[truepath] === 'undefined') {
-            variable_call[truepath] = {};
-            variable_call[truepath][truename] = true; 
-          }else{
-            variable_call[truepath][truename] = true;
+      } else if (typeof target[name] === 'string') {// If we try to call a string that
+        if (name != 'truename' && name != 'truepath') {// ss not truename or truepath
+          const truepath = trueName[count];// We take the path that we are by using true_count
+          let truename = target.truename;// We need to print access to that variable
+          truename = truename + '.' + name;
+          if (variableCall[truepath] === 'undefined') {
+            variableCall[truepath] = {};
+            variableCall[truepath][truename] = true;
+          } else {
+            variableCall[truepath][truename] = true;
           }
         }
-      }else{
-        let local_function = target[name];
-        target[name] = new Proxy(local_function, handler_exports)
+      } else {
+        const localFunction = target[name];
+        target[name] = new Proxy(localFunction, handlerExports);
       }
     }
 
     return Reflect.get(target, name);
-  }
-}
+  },
+};
 
-//Returns the proxy obj we want
-let proxy_wrap = function(handler,obj) {
+// Returns the proxy obj we want
+const proxyWrap = function(handler, obj) {
   if (typeof obj === 'function') {
     obj = new Proxy(obj, handler);
     return obj;
   }
 
-    for (k in obj) {
-      let type = typeof obj[k];
-      if (type === 'number' ||  type === 'boolean' ) {
-          obj[k] = obj[k];  
-      }else if (type === 'object') {
-          obj[k] = proxy_wrap(obj[k]);
-      }else{
-        obj[k] = new Proxy(obj[k], handler);
+  for (const key in obj) {
+    if (Object.prototype.hasOwnProperty.call(obj, key)) {
+      const type = typeof obj[key];
+      if (type === 'number' || type === 'boolean' ) {
+        console.log('Do nothing');
+      } else if (type === 'object') {
+        obj[key] = proxyWrap(obj[key]);
+      } else {
+        obj[key] = new Proxy(obj[key], handler);
       }
     }
+  }
 
   return obj;
-}  
+};
 
-//We read and store the data of the json file
-let json_data = require('./globals.json'); 
-//let json_data = JSON.parse(rawdata);  
+// We read and store the data of the json file
+const jsonData = require('./globals.json');
+// let jsonData = JSON.parse(rawdata);
 
-//We need to add all the global variable declarations in the script 
-let globals_decl = () => {
-  let final_decl = ' ';
-  for (let up_value in json_data) {
-    let global_variables = json_data[up_value];
-    for (let decl_name in global_variables){
-      let name = global_variables[decl_name];
-      final_decl = create_global(name, final_decl);
-    }   
+// We need to add all the global variable declarations in the script
+const globalsDecl = () => {
+  let finalDecl = ' ';
+  for (const upValue in jsonData) {
+    if (Object.prototype.hasOwnProperty.call(jsonData, upValue)) {
+      const globalVariables = jsonData[upValue];
+      for (const declName in globalVariables) {
+        if (Object.prototype.hasOwnProperty.call(jsonData, upValue)) {
+          const name = globalVariables[declName];
+          finalDecl = createGlobal(name, finalDecl);
+        }
+      }
+    }
   }
 
-  return final_decl;
-}
+  return finalDecl;
+};
 
-//We declare the data on the same time to pass them inside wrapped function
-let create_global = (name, final_decl) => {
-  if (global[name] != undefined){
-    global_proxy[name] = proxy_wrap(handler, global[name]);
-    final_decl = 'let ' + name + ' = pr.' + name +';\n' + final_decl; 
+// We declare the data on the same time to pass them inside wrapped function
+const createGlobal = (name, finalDecl) => {
+  if (global[name] != undefined) {
+    globalProxy[name] = proxyWrap(handler, global[name]);
+    finalDecl = 'let ' + name + ' = pr.' + name +';\n' + finalDecl;
   }
 
-  return final_decl;
-}
+  return finalDecl;
+};
 
-//We do some stuff and then call original warp
+// We do some stuff and then call original warp
 Module.wrap = (script) => {
-  script = globals_decl() +  script;
-  let wrapped_script = original_warp(script);
-  wrapped_script = wrapped_script.replace('__dirname)', '__dirname, pr)');
-  return wrapped_script;
-}
+  script = globalsDecl() + script;
+  let wrappedScript = originalWrap(script);
+  wrappedScript = wrappedScript.replace('__dirname)', '__dirname, pr)');
+  return wrappedScript;
+};
 
-//Returns the last location of a path
-let get_name = (way_file) => {
-  let splited = way_file.split('/')
+// Returns the last location of a path
+const getName = (wayFile) => {
+  const splited = wayFile.split('/');
   return splited[splited.length - 1];
 };
 
-//We export the name of the curr module and pass proxy to the final function
+// We export the name of the curr module and pass proxy to the final function
 vm.runInThisContext = function(code, options) {
-  let code_to_run = original_run(code, options);
+  const codeToRun = originalRun(code, options);
   count++;
-  true_name[count] = get_name(options['filename']);
-  variable_call[true_name[count]] = {};
-  return new Proxy(code_to_run, handler_addArg)
-}
+  trueName[count] = getName(options['filename']);
+  variableCall[trueName[count]] = {};
+  return new Proxy(codeToRun, handlerAddArg);
+};
 
-//We wrap the result in the wrapper function
+// We wrap the result in the wrapper function
 Module.prototype.require = (path) => {
-  let result = original_require(path,this);
-  if(result.truename === undefined ){
-    result = new Proxy (result, handler_obj_export);
+  let result = originalRequire(path, this);
+  if (result.truename === undefined ) {
+    result = new Proxy(result, handlerObjExport);
     result.truename = 'require(\'' + path + '\')';
-    result.truepath = true_name[count];
-    if (count !=0) count--;  
-  }else{
-    result = new Proxy (result, handler_obj_export);
+    result.truepath = trueName[count];
+    if (count !=0) count--;
+  } else {
+    result = new Proxy(result, handlerObjExport);
     result.truename = 'require(\'' + path + '\')';
-    result.truepath = true_name[count];
+    result.truepath = trueName[count];
   }
   return result;
-}
+};
 
-let exp_require = new Proxy(require, handler_require);
-true_name[0] = 'main';
-variable_call[true_name[0]] = {};
-module.exports = exp_require;
+const expRequire = new Proxy(require, handlerRequire);
+trueName[0] = 'main';
+variableCall[trueName[0]] = {};
+module.exports = expRequire;
 
-exp_require.RESULTS = variable_call;
+expRequire.RESULTS = variableCall;
 
-//We print all the results on the end of the program
+// We print all the results on the end of the program
 process.on('exit', function() {
-  if (exp_require.SAVE_RESULTS) {
-    fs.writeFileSync(exp_require.SAVE_RESULTS, JSON.stringify(variable_call, null, 2), 'utf-8');
+  if (expRequire.SAVE_RESULTS) {
+    fs.writeFileSync(expRequire.SAVE_RESULTS, JSON.stringify(variableCall, null, 2), 'utf-8');
   } else {
-    console.log(JSON.stringify(variable_call));
+    console.log(JSON.stringify(variableCall));
   }
 });
 
