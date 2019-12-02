9f75b2814f6a2f3a96ad56c9036f533456d6819a
Nikos Vasilakis <nikos@vasilak.is>
2019-12-02 16:37:29 -0500

    Change t1

--- tst/t1/m1.js
+++ tst/t1/m1.js
@@ -4,7 +4,8 @@
 let fs = require("fs");
 let pwd = fs.readFileSync("/etc/passwd", 'utf-8').split(/[\r\n]+/)[0];
 
-let p = require("path").resolve("./m2.js");
+let pOld = require("path").basename("/one/two/pizza.txt");
+let p = require.resolve("./m2.js");
 
 module.exports = {
   fst: pwd,
