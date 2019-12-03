46ad67b27da2d75c45d4fbaa42cac09e89f7e024
GNtousakis <greco1996@gmail.com>
2019-12-03 18:49:46 +0200

    some comments on the problems

--- tst/t3/m1.js
+++ tst/t3/m1.js
@@ -4,22 +4,22 @@
 // "EvalError",
 
 const arr = Array.of(1, 2, 3);
-const buffer = new SharedArrayBuffer(16);
-//const buffer = new ArrayBuffer(16);
-const uint8 = new Uint8Array(buffer);
-Atomics.add(uint8, 0, 2);
+//const buffer = new SharedArrayBuffer(16);
+const buffer = new ArrayBuffer(16);
+const uint8 = new Uint8Array(arr);
+//Atomics.add(uint8, 0, 2);
 
-const alsoHuge = BigInt(9007199254740991);
-const bigint64 = new BigInt64Array(2);
+//const alsoHuge = BigInt(9007199254740991);
+//const bigint64 = new BigInt64Array(2);
 const b = new Boolean(false);
 
-//	"constructor",
+//	"constructor", Works
 const view1 = new DataView(buffer);
 const date1 = new Date('December 17, 1995 03:24:00');
 const uri = 'https://mozilla.org/?x=шеллы';
 const r = encodeURI( encodeURIComponent( decodeURI( encodeURI(uri) ) ) );
 
-// does one of the two not work?
+// Both works!
 const x = Error('I was created using a function call!');
 const y = new Error('I was constructed via the "new" keyword!');
 const er = eval('2 + 2');
@@ -71,3 +71,10 @@ const er = eval('2 + 2');
 //	"valueOf",
 //	"WeakMap",
 //	"WeakSet"
+
+// Not compatible with Node v8.9.4 --version we are using
+// *******************************************************************************
+// "Atomics" -> 8.10.0
+// "SharedArrayBuffer" -> 8.10.0
+// "BigInt"  -> 10.4.0
+// "BigInt64Array" -> 10.4.0
