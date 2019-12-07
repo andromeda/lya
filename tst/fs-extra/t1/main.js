c5e4c5c31c5fbd2ceec2cd9ee507cc3fdccbae13
GNtousakis <greco1996@gmail.com>
2019-12-07 11:45:06 +0200

    Add fs-extra

---
+++ tst/fs-extra/t1/main.js
@@ -0,0 +1,39 @@
+lyaConfig = {
+  SAVE_RESULTS: require("path").join(__dirname, "dynamic.json"),
+  analysisCh: 1,
+};
+let lya = require("../../../src/txfm.js");
+require = lya.configRequire(require, lyaConfig);
+
+const fs = require('fs-extra')
+ 
+// Async with promises:
+fs.copy('/tmp/myfile', '/tmp/mynewfile')
+  .then(() => console.log('success!'))
+  .catch(err => console.error(err))
+ 
+// Async with callbacks:
+fs.copy('/tmp/myfile', '/tmp/mynewfile', err => {
+  if (err) return console.error(err)
+  console.log('success!')
+})
+ 
+// Sync:
+try {
+  fs.copySync('/tmp/myfile', '/tmp/mynewfile')
+  console.log('success!')
+} catch (err) {
+  console.error(err)
+}
+ 
+// Async/Await:
+async function copyFiles () {
+  try {
+    await fs.copy('/tmp/myfile', '/tmp/mynewfile')
+    console.log('success!')
+  } catch (err) {
+    console.error(err)
+  }
+}
+ 
+copyFiles()
