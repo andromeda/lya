2356b5db2a67684a15c06cbeea6494423a6f856c
GNtousakis <greco1996@gmail.com>
2019-12-07 11:24:25 +0200

    Add commander

---
+++ tst/commander/t1/main.js
@@ -0,0 +1,9 @@
+lyaConfig = {
+  SAVE_RESULTS: require("path").join(__dirname, "dynamic.json"),
+  analysisCh: 1,
+};
+let lya = require("../../../src/txfm.js");
+require = lya.configRequire(require, lyaConfig);
+
+const program = require('commander');
+program.version('0.0.1');
