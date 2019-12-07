d5b6cab9a2cebb8f5c4af529224ce8961eb5890d
GNtousakis <greco1996@gmail.com>
2019-12-07 19:31:51 +0200

    Update shell script

--- tst/run-tests.sh
+++ tst/run-tests.sh
@@ -3,22 +3,56 @@ set -e
 # or call the script with its absolute name
 cd $(dirname $0)
 
-cd ./t1
-node test.js
-echo "Test 1 .... ok"
+cd ./chalk/t1
+node main.js
+echo "chalk - Test 1 .... ok"
 cd ..
 
 cd ./t2
-node test.js
-echo "Test 2 .... ok"
+node main.js
+echo "chalk - Test 2 .... ok"
+cd ../../
+
+cd ./commander/t1
+node main.js
+echo "commander - Test 1 .... ok"
+cd ../../
+
+cd ./lodash/t1
+node main.js
+echo "lodash - Test 1 .... ok"
+cd ../t2
+
+node main.js
+echo "lodash - Test 2 .... ok"
+cd ../../
+
+cd ./moment/t1
+node main.js
+echo "moment - Test 1 .... ok"
+cd ../../
+
+cd ./t1
+node main.js
+echo "general - Test 1 .... ok"
+cd ..
+
+cd ./t2
+node main.js
+echo "general - Test 2 .... ok"
 cd ..
 
 cd ./t3
-node test.js
-echo "Test 3 .... ok"
+node main.js
+echo "general - Test 3 .... ok"
 cd ..
 
 cd ./t4
-node test.js
-echo "Test 4 .... ok"
+node main.js
+echo "general - Test 4 .... ok"
+cd ..
+
+cd ./t5
+node main.js
+echo "general - Test 5 .... ok"
 cd ..
