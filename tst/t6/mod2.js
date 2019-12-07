08460603ce8af9b8514dda34c205ac8f1f703e7f
Nikos Vasilakis <nikos@vasilak.is>
2019-12-07 09:41:34 -0500

    Add new test-case

---
+++ tst/t6/mod2.js
@@ -0,0 +1,14 @@
+const mod3 = require("./mod3.js");
+const mod4 = require("./mod4.js");
+
+const run = function run() {
+  for (let i = 0; i < 100000; i++) {
+    if (Math.random() > 0.25) {
+      mod3.run();
+    } else {
+      mod4.run();
+    }
+  }
+};
+
+module.exports = {run};
---
+++ tst/t6/mod3.js
@@ -0,0 +1,5 @@
+const run = function run() {
+  for (let i = 0; i < 10000; i++);
+};
+
+module.exports = {run};
