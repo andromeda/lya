aa151bc2aa0b225b0021517bf92591c84a93de74
GNtousakis <greco1996@gmail.com>
2019-12-08 15:14:57 +0200

    Add color repo -needs fix

---
+++ tst/color/t1/main.js
@@ -0,0 +1,14 @@
+lyaConfig = {
+  SAVE_RESULTS: require("path").join(__dirname, "dynamic.json"),
+  analysisCh: 1,
+};
+let lya = require("../../../src/txfm.js");
+require = lya.configRequire(require, lyaConfig);
+
+var colors = require('colors');
+ 
+console.log('hello'.green); // outputs green text
+console.log('i like cake and pies'.underline.red) // outputs red underlined text
+console.log('inverse the color'.inverse); // inverses the color
+console.log('OMG Rainbows!'.rainbow); // rainbow
+console.log('Run the trap'.trap); // Drops the bass
