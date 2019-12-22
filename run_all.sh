485b31a5dcbd73d71079b8f18d49c46a958b562c
GNtousakis <greco1996@gmail.com>
2019-12-22 23:45:29 +0200

    Update script to run tests

--- run_all.sh
+++ run_all.sh
@@ -118,3 +118,68 @@ npm --key=6  test > /dev/null
 npm --key=7  test > /dev/null
 echo "We finish all test for yargs"
 
+cd ../../synthetic
+cd t1
+[ -e timetest.txt ] && rm timetest.txt
+
+key=0 node main.js > /dev/null
+key=1 node main.js > /dev/null
+key=5 node main.js > /dev/null
+key=2 node main.js > /dev/null
+key=3 node main.js > /dev/null
+key=4 node main.js > /dev/null
+key=6 node main.js > /dev/null
+key=7 node main.js > /dev/null
+echo "The 1st synthetic test.... ok"
+
+cd ../t2
+[ -e timetest.txt ] && rm timetest.txt
+
+key=0 node main.js > /dev/null
+key=1 node main.js > /dev/null
+key=5 node main.js > /dev/null
+key=2 node main.js > /dev/null
+key=3 node main.js > /dev/null
+key=4 node main.js > /dev/null
+key=6 node main.js > /dev/null
+key=7 node main.js > /dev/null
+echo "The 2nd synthetic test.... ok"
+
+cd ../t3
+[ -e timetest.txt ] && rm timetest.txt
+
+key=0 node main.js > /dev/null
+key=1 node main.js > /dev/null
+key=5 node main.js > /dev/null
+key=2 node main.js > /dev/null
+key=3 node main.js > /dev/null
+key=4 node main.js > /dev/null
+key=6 node main.js > /dev/null
+key=7 node main.js > /dev/null
+echo "The 3rd synthetic test.... ok"
+
+cd ../t4
+[ -e timetest.txt ] && rm timetest.txt
+
+key=0 node main.js > /dev/null
+key=1 node main.js > /dev/null
+key=5 node main.js > /dev/null
+key=2 node main.js > /dev/null
+key=3 node main.js > /dev/null
+key=4 node main.js > /dev/null
+key=6 node main.js > /dev/null
+key=7 node main.js > /dev/null
+echo "The 4th synthetic test.... ok"
+
+cd ../t5
+[ -e timetest.txt ] && rm timetest.txt
+
+key=0 node main.js > /dev/null
+key=1 node main.js > /dev/null
+key=5 node main.js > /dev/null
+key=2 node main.js > /dev/null
+key=3 node main.js > /dev/null
+key=4 node main.js > /dev/null
+key=6 node main.js > /dev/null
+key=7 node main.js > /dev/null
+echo "The 5th synthetic test.... ok"
--- tst/working/synthetic/t1/main.js
+++ tst/working/synthetic/t1/main.js
@@ -1,10 +1,22 @@
-lyaConfig = {
-  SAVE_RESULTS: require("path").join(__dirname, "dynamic.json"),
-  POLICY: require("path").join(__dirname, "dynamic.json"),
-  analysisCh: 6,
-};
 
-let lya = require("../../../../src/txfm.js");
-require = lya.configRequire(require, lyaConfig);
+if (parseInt(process.env.key) != 0) {
+        lyaConfig = {
+        SAVE_RESULTS: require("path").join(__dirname, "dynamic.json"),
+        analysisCh: parseInt(process.env.key),
+        removejson: ['hasOwnProperty'],
+        };
+        let lya = require("../../../../src/txfm.js");
+        require = lya.configRequire(require, lyaConfig);
+}
+const time = process.hrtime();
+const fs = require('fs');
 
 const m1 = require('./m1.js');
+
+const diff = process.hrtime(time);
+const thisTime = (diff[0] * 1e9 + diff[1]) * 1e-6;
+var logger = fs.createWriteStream('timetest.txt', {
+  flags: 'a' // 'a' means appending (old data will be preserved)
+})
+logger.write('The time of ' + parseInt(process.env.key) + ' is ' + thisTime + ' \n', 'utf-8');
+
