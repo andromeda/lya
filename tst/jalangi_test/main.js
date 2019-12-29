8364a5a46716da89d06ea9bc185f2fa3aff7aea1
GNtousakis <greco1996@gmail.com>
2019-12-29 19:20:39 +0200

    Small Jalangi test

---
+++ tst/jalangi_test/main.js
@@ -0,0 +1,8 @@
+lyaConfig = {
+SAVE_RESULTS: require("path").join(__dirname, "dynamic.json"),
+analysisCh: 8,
+};
+let lya = require("../../src/txfm.js");
+require = lya.configRequire(require, lyaConfig);
+
+require("./3d-cube.js");
