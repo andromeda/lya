9f8694871f09b579405b7839f0bf2d8e3f29fcb5
GNtousakis <greco1996@gmail.com>
2020-01-03 10:46:43 +0200

    Rerun tests

--- tst/working/synthetic/t6/main.js
+++ tst/working/synthetic/t6/main.js
@@ -1,6 +1,6 @@
 lyaConfig = {
     SAVE_RESULTS: require("path").join(__dirname, "dynamic.json"),
-    analysisCh: 1,
+    analysisCh: 6,
 };
 let lya = require("../../../../src/txfm.js");
 require = lya.configRequire(require, lyaConfig);
