3bf42d2414b6db122cab2faf5f7fbc907c6b105a
GNtousakis <greco1996@gmail.com>
2020-01-10 17:39:24 +0000

    Fix

--- tst/nodejs_apps/moeda/bin/index.js
+++ tst/nodejs_apps/moeda/bin/index.js
@@ -14,8 +14,11 @@ const argv = process.argv.slice(2)
 helpers(argv)
 
 const command = {
-  amount: argv[0] || 1,
-  from: argv[1] || 'USD',
-  to: (argv.length > 2) ? process.argv.slice(4) : ['USD', 'EUR', 'GBP', 'BRL']
+  amount: 1,
+  from: 'USD',
+  to: ['USD', 'EUR', 'GBP', 'BRL']
 }
-moeda(command)
+
+//for (i = 0; i < 1000; i++) {
+  moeda(command)
+//}
