41a33d888f8e3ecf3d0cb0a7ca2b1677b1c190b4
GNtousakis <greco1996@gmail.com>
2020-03-10 15:50:37 +0200

    Updated to catch R in (math.fft etc)

--- src/policy6.js
+++ src/policy6.js
@@ -34,7 +34,7 @@ const addEvent = (event, values, index) => {
 
 // @storedCalls it is a table that contains all the analysis data
 // @truename the name of the current function, object etc that we want to add to
-// the table 
+// the table
 // @mode the mode of the current access (R,W or E)
 // Given those two inputs we can update the analysis data that are stored in storedCalls
 const updateAnalysisData = (storedCalls, truename, mode) => {
@@ -46,7 +46,7 @@ const updateAnalysisData = (storedCalls, truename, mode) => {
   }
 };
 
-// The handler of the global variable.Every time we access the global variabe in order to declare 
+// The handler of the global variable.Every time we access the global variabe in order to declare
 // or call a variable, then we can print it on the export file.
 const globalHandler = {
   get: function(target, name) {
@@ -79,8 +79,8 @@ const globalHandler = {
 };
 
 // The handler of the all the function that are called inside a module. Every time we
-// load a module with require it first execute all the code and then prepary and exports 
-// all the export data. We use this handler to catch all the code that is executed on the 
+// load a module with require it first execute all the code and then prepary and exports
+// all the export data. We use this handler to catch all the code that is executed on the
 // module.
 const moduleHandler = {
   apply: function(target) {
@@ -97,7 +97,7 @@ const moduleHandler = {
   },
 };
 
-// The handler of the functions on the export module. Every time we require a module 
+// The handler of the functions on the export module. Every time we require a module
 // and we have exports, we wrap them in a handler. Each time we call a function from inside
 // exports this is the handler that we wrap the function.
 const exportsFuncHandler = {
@@ -114,22 +114,29 @@ const exportsFuncHandler = {
 };
 
 // Read function so we print it in the export file
-// This is to catch the read
+// This is to catch the read of a called function
+// require("math.js").fft && require("math.js").fft.mult
 const readFunction = (myFunc, name) => {
-  name = name + '.' + myFunc.name;
+
+  const wholeName = name + '.' + myFunc.name;
   const currentPlace = locEnv.trueName[locEnv.requireLevel];
   let storedCalls = locEnv.accessMatrix[currentPlace];
 
   if (Object.prototype.hasOwnProperty.
         call(storedCalls, name) === false) {
     storedCalls[name] = 'R';
+  }
+
+  if (Object.prototype.hasOwnProperty.
+        call(storedCalls, wholeName) === false) {
+    storedCalls[wholeName] = 'R';
   } else {
-    addEvent('R', storedCalls, name);
+    addEvent('R', storedCalls, wholeName);
   }
 };
 
-// This is the handler of the global constanst variables, like Math.PI etc. We store the name 
-// in the same object but we use a different name, for example, for Math.PI we store the 
+// This is the handler of the global constanst variables, like Math.PI etc. We store the name
+// in the same object but we use a different name, for example, for Math.PI we store the
 // name "Math.PI" in the object Math.PIPI. That way we can have accurate name analysis.
 const globalConstHandler = {
   get: function(target, name) {
