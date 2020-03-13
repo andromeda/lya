5452581c9c0ec117a1cd51f9115053eb6733a4af
Nikos Vasilakis <nikos@vasilak.is>
2020-03-13 15:15:58 -0400

    Proof of concept for wrapping globals

---
+++ src/wrapGlobals.js
@@ -0,0 +1,28 @@
+
+// https://2ality.com/2016/11/trace-globals-proxy.html
+let _glob;
+if (typeof global !== 'undefined') {
+  _glob = global; 
+  console.log("pointing _glob to global");
+} else {
+  _glob = self;
+  console.log("pointing _glob to self");
+}
+
+function evalCode(code) {
+    const func = new Function ("proxy", "with (proxy) {" + code + "}");
+    const proxy = new Proxy(_glob, {
+        get(target, propKey, receiver) {
+            console.log(`GET ${String(propKey)}`); // (B)
+            return Reflect.get(target, propKey, receiver);
+        },
+        set(target, propKey, value, receiver) { // (C)
+            console.log(`SET ${String(propKey)}=${value}`);
+            return Reflect.set(target, propKey, value, receiver);
+        },
+    });
+    return func(proxy);
+}
+
+evalCode('x = 3')
+console.log(x)
