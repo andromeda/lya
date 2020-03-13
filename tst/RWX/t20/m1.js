1e9982eb848996a76160d120abbd3bbba3268590
Nikos Vasilakis <nikos@vasilak.is>
2020-03-13 11:51:25 -0400

    Add a false positives

---
+++ tst/RWX/t20/m1.js
@@ -0,0 +1,9 @@
+var process = {env: {}}
+
+process.env.CHAN = 3
+process.env.PWD  = "?"
+process.env.HOME  = "?"
+process.env.USER  = "?"
+process.env.SHELL  = "?"
+process.env.PATH  = "?"
+process.env.CHAN  = "?"
