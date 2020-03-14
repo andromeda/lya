daa41a66e5f076946f2506834a833b7542c6fe51
GNtousakis <greco1996@gmail.com>
2020-03-14 20:46:47 +0200

    Test for Math.E,ln10 etc

---
+++ tst/synthetic/t13/main.js
@@ -0,0 +1,8 @@
+lyaConfig = {
+SAVE_RESULTS: require("path").join(__dirname, "dynamic.json"),
+analysisCh: 6,
+};
+let lya = require("../../../src/txfm.js");
+require = lya.configRequire(require, lyaConfig);
+
+require('./m1.js')
