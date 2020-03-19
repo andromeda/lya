f06b5de029d5cf781ee433bde282787b9027c357
Nikos Vasilakis <nikos@vasilak.is>
2020-03-19 17:30:08 -0400

    Add templates

--- core/tst/RWX/run-static.sh
+++ core/tst/RWX/run-static.sh
@@ -5,5 +5,6 @@ set -e
 for d in t*/; do
   cd $d;
   java -jar ../../mir-sa.jar . | grep "^{" | jq .  > RWX.static.json
+  node -e 'var eq = require("lodash.isequal"); var c = require("./RWX.correct.json"); var d = require("./RWX.static.json"); if (!eq(c, d)) { console.log(require("json-diff").diffString(c, d)); process.exit(-1); }'
   cd ..
 done
