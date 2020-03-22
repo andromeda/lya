9e81019cffc57d77e80e0eec3911b9238d4edd8c
Nikos Vasilakis <nikos@vasilak.is>
2020-03-22 16:32:25 -0400

    Fix t2

--- core/tst/RWX/t2/main.js
+++ core/tst/RWX/t2/main.js
@@ -1,10 +1,5 @@
 const fs = require('fs');
-
-// Un-commenting  the line below will fail the test
-//let pwd = require('fs').readFileSync('/etc/passwd');
-
 let m1 = require("./m1.js");
-console.log('This is test 2')
 let m2 = require("./m2.js");
 
 if (m1.fst === [m2.fst, m1.fst, m2.fst]) {
