031e7fc4e94ae6d0597d780dc4cf20bf1cdbca8d
Nikos Vasilakis <nikos@vasilak.is>
2020-03-29 16:09:39 -0400

    Wrapper captures built-in globals

--- core/src/wrapGlobals.js
+++ core/src/wrapGlobals.js
@@ -22,11 +22,20 @@ function sandboxJS(js) {
 
 // sandboxJS("console.log(2)");        // 2
 // sandboxJS("console.log(history)");  // Error, Not allowed: history
+console.log("x = 3");
 sandboxJS('x = 3');
-console.log('=====');
+
+console.log("x = 4");
 sandboxJS('x = 4');
-console.log('=====');
+
+console.log('console.log(x)');
 sandboxJS('console.log(x)');
+
+console.log('Infinity');
+sandboxJS('Infinity');
+
+console.log('process');
+sandboxJS('process');
 
 /*
 Examples of closures etc
