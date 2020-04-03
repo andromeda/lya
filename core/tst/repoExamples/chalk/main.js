76634205e2dddbc848b721fc3c811261ca18a7be
GNtousakis <greco1996@gmail.com>
2020-04-03 20:48:25 +0300

    Add examples for chalk, minimist and yargs

---
+++ core/tst/repoExamples/chalk/main.js
@@ -0,0 +1,56 @@
+const chalk = require('chalk');
+const log = console.log;
+
+console.log(chalk.blue('Hello world!'));
+
+// Combine styled and normal strings
+log(chalk.blue('Hello') + ' World' + chalk.red('!'));
+ 
+// Compose multiple styles using the chainable API
+log(chalk.blue.bgRed.bold('Hello world!'));
+ 
+// Pass in multiple arguments
+log(chalk.blue('Hello', 'World!', 'Foo', 'bar', 'biz', 'baz'));
+ 
+// Nest styles
+log(chalk.red('Hello', chalk.underline.bgBlue('world') + '!'));
+ 
+// Nest styles of the same type even (color, underline, background)
+log(chalk.green(
+    'I am a green line ' +
+    chalk.blue.underline.bold('with a blue substring') +
+    ' that becomes green again!'
+));
+ 
+// ES2015 template literal
+log(`
+CPU: ${chalk.red('90%')}
+RAM: ${chalk.green('40%')}
+DISK: ${chalk.yellow('70%')}
+`);
+ 
+ 
+// Use RGB colors in terminal emulators that support it.
+log(chalk.keyword('orange')('Yay for orange colored text!'));
+log(chalk.rgb(123, 45, 67).underline('Underlined reddish color'));
+log(chalk.hex('#DEADED').bold('Bold gray!'));
+
+const error = chalk.bold.red;
+const warning = chalk.keyword('orange');
+
+console.log(error('Error!'));
+console.log(warning('Warning!'));
+
+const name = 'Sindre';
+console.log(chalk.green('Hello %s'), name);
+
+const miles = 18;
+const calculateFeet = miles => miles * 5280;
+
+console.log(chalk`
+    There are {bold 5280 feet} in a mile.
+    In {bold ${miles} miles}, there are {green.bold ${calculateFeet(miles)} feet}.
+`);
+
+console.log(chalk.bold.rgb(10, 100, 200)('Hello!'));
+console.log(chalk`{bold.rgb(10,100,200) Hello!}`);
