63e95dbfa500dddb9d8a6a46bf1b1f9e5289e426
GNtousakis <greco1996@gmail.com>
2020-04-06 23:53:13 +0300

    Error on chalk even with Lya options disabled

--- core/tst/repoExamples/chalk/main.js
+++ core/tst/repoExamples/chalk/main.js
@@ -5,13 +5,13 @@ console.log(chalk.blue('Hello world!'));
 
 // Combine styled and normal strings
 log(chalk.blue('Hello') + ' World' + chalk.red('!'));
- 
+
 // Compose multiple styles using the chainable API
 log(chalk.blue.bgRed.bold('Hello world!'));
- 
+
 // Pass in multiple arguments
 log(chalk.blue('Hello', 'World!', 'Foo', 'bar', 'biz', 'baz'));
- 
+
 // Nest styles
 log(chalk.red('Hello', chalk.underline.bgBlue('world') + '!'));
  
@@ -21,15 +21,15 @@ log(chalk.green(
     chalk.blue.underline.bold('with a blue substring') +
     ' that becomes green again!'
 ));
- 
+
 // ES2015 template literal
 log(`
 CPU: ${chalk.red('90%')}
 RAM: ${chalk.green('40%')}
 DISK: ${chalk.yellow('70%')}
 `);
- 
- 
+
+
 // Use RGB colors in terminal emulators that support it.
 log(chalk.keyword('orange')('Yay for orange colored text!'));
 log(chalk.rgb(123, 45, 67).underline('Underlined reddish color'));
