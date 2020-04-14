8f4a1d5d46cca47135658b3c1167b2b94729f265
GNtousakis <greco1996@gmail.com>
2020-04-14 18:55:01 +0300

    Small corrections on run-checking

--- core/tst/RWX/run-checking-all.sh
+++ core/tst/RWX/run-checking-all.sh
@@ -8,10 +8,12 @@ runChecking() {
   cat ../prologue-check.lya | sed "s;REPLACE_ME;$json;" > prologue.lya
   cat prologue.lya ../epilogue.lya > generated.test
   rm prologue.lya
-  (node generated.test) > ../test.txt
+  node generated.test
 }
 
 loopAll() {
+  testNumber=$1
+  echo "Test number: $1"
   for name in correct dynamic static; do
     $RUN_CHECKING $name
   done
@@ -19,12 +21,12 @@ loopAll() {
 
 if [ "$#" -eq 1 ]; then
   cd $1
-  $LOOP_ALL
+  $LOOP_ALL $#
   cd ..
 else
   for d in t[3456789]?/ t???/; do
     cd $d;
-    $LOOP_ALL
+    $LOOP_ALL $d
     cd ..
   done
 fi
