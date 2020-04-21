2c1badcc18d4f6889f27faf5d1fadd3001f2813a
Nikos Vasilakis <nikos@vasilak.is>
2020-04-21 07:42:36 -0400

    Update static analysis

--- core/tst/RWX/run-check.sh
+++ core/tst/RWX/run-check.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+MIR_SA=${MIR_PATH:-~/wrk/andromeda/mir/static-analysis/mir-sa.jar}
 GROUND_TRUTH="${GROUND_TRUTH:-static}" # ground truth can be: static, dynamic, correct
 
 analysis() {
@@ -7,6 +8,13 @@ analysis() {
   # static analysis does not get confused
   t="$(echo $1 | sed 's;/;;' | sed 's;$;:;')"
   cat correct.pwd.json | sed "s;PWD_REPLACE;$(pwd);" > correct.json
+
+  # generate static
+  java -jar $MIR_SA . | grep "^{" | jq .  > static.json
+  # java -Dmaybe.reaching=true -jar $MIR_SA . | grep "^{" | jq .  > static.json
+  # java -Dbase.stars=true -jar $MIR_SA . | grep "^{" | jq .  > static.json
+  # java -Dprop.stars=true -jar $MIR_SA . | grep "^{" | jq .  > static.json
+
   cat ../prologue-check.lya ../epilogue.lya | sed "s/GROUND_TRUTH/$GROUND_TRUTH/" > generated.test
   # Replace node with cat to see the generated script
   node generated.test 2>&1 > /dev/null | sed "s;^;$t  ;" | grep correct
