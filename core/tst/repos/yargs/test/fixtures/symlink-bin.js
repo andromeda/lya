957771060e13dff980cd467317ec3f0a5a4aa62d
GNtousakis <greco1996@gmail.com>
2020-04-24 21:47:47 +0300

    More cleanup

--- core/tst/repos/yargs/test/fixtures/symlink-bin.js
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env node
-var argv = require('./yargs-symlink/index.js')
-  .help('help')
-  .version()
-  .argv
-console.log(argv)
--- core/tst/repos/yargs/test/fixtures/test/fixtures/yargs/normal-bin.js
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env node
-var argv = require('./yargs/index.js')
-  .help('help')
-  .version()
-  .argv
-console.log(argv)
