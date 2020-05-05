3acf6cb076a2eedacc1ce5a1392978323426ca1f
GNtousakis <greco1996@gmail.com>
2020-05-05 01:33:26 -0400

    Small update

--- paper/tst/jalangi_test/run.sh
+++ paper/tst/jalangi_test/run.sh
@@ -33,6 +33,22 @@ do
 	name=$line.js
 	echo "******************************"
   	echo "We are processing this: $name"
+  	sed -i "s/${prevname}/${name}/" noWith.js
+  	echo "The name of file $name" >> timeResultsLyaNOWITH.txt
+  	(time node noWith.js) >>  /dev/null 2>> "timeResultsLyaNOWITH.txt"
+  	prevname=$name
+
+done < "$input"
+
+sed -i "s/${prevname}/replaceme.js/" noWith.js
+
+prevname="replaceme.js"
+input="./sunspider/LIST"
+while IFS= read -r line
+do
+	name=$line.js
+	echo "******************************"
+  	echo "We are processing this: $name"
   	sed -i "s/${prevname}/${name}/" main.js
   	echo "The name of file $name" >> timeResultsLya.txt
   	(time node main.js) >>  /dev/null 2>> "timeResultsLya.txt"
@@ -46,18 +62,4 @@ echo "******************************"
 echo "******************************"
 echo "We are going to run the test for programs with lya but noWith"
 
-prevname="replaceme.js"
-input="./sunspider/LIST"
-while IFS= read -r line
-do
-	name=$line.js
-	echo "******************************"
-  	echo "We are processing this: $name"
-  	sed -i "s/${prevname}/${name}/" noWith.js
-  	echo "The name of file $name" >> timeResultsLyaNOWITH.txt
-  	(time node noWith.js) >>  /dev/null 2>> "timeResultsLyaNOWITH.txt"
-  	prevname=$name
 
-done < "$input"
-
-sed -i "s/${prevname}/replaceme.js/" noWith.js
