6ef24b05d2a3d88501034b104b4f83059f732cb8
GNtousakis <greco1996@gmail.com>
2020-05-11 11:35:58 +0300

    Script runs 4 analysis for time results

--- core/tst/micro-packages/run-perf.sh
+++ core/tst/micro-packages/run-perf.sh
@@ -8,7 +8,8 @@ analysis() {
   cd $1
   t="$(echo $1 | sed 's;/;;' | sed 's;$;:;')"
   m=$(cat package.json | jq .main | tr -d '"')
-  
+  type=$2
+
   # If entry file exists, rename it
   nm="$(dirname $m)/$PRE$(basename $m)"
   [ ! -f $nm ] && mv $m $nm
@@ -18,7 +19,7 @@ let lya = require("$LYA_BASE");
 let conf = {
   debugName: '$t',
   timerStart: process.hrtime(),
-  analysis: lya.preset.RWX_PERFORMANCE,
+  analysis: lya.preset.$type,
   context: {
     excludes: ['Promise', 'toString', 'escape', 'setImmediate'],
   },
@@ -34,7 +35,7 @@ PROLOGUE
   echo "$PLG" | tee $m
 
   # npm test 2>&1 > /dev/null | sed "s;^;$t  ;" | grep correct
-  echo "Time $t" >> ../time
+  echo "Time $t $type" >> ../time
   loopNumber=100
   for ((i=0; i<=loopNumber; i=i+1))
   do
@@ -47,13 +48,15 @@ PROLOGUE
   # node -e 'var eq = require("lodash.isequal"); var c = require("./correct.json"); var d = require("./dynamic.json"); if (!eq(c, d)) { console.log(require("json-diff").diffString(c, d)); process.exit(-1); }' | nl
 }
 
-if [ "$#" -eq 1 ]; then
-  analysis $1
-else
-  for d in */; do
-    analysis $d
-  done
-fi
+for a in CALL_NUMBERS PROFILING_RELATIVE GLOBAL_ONLY RWX_PERFORMANCE; do
+  if [ "$#" -eq 1 ]; then
+    analysis $1 $a
+  else
+    for d in */; do
+      analysis $d $a
+    done
+  fi
+done 
 
 grep Time time > timeOnly.txt
 
