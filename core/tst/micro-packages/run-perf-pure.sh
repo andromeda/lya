13508cd7db016464e1e116437bef4da3ce2af6b9
GNtousakis <greco1996@gmail.com>
2020-05-11 13:31:19 -0400

    Update script to run pure tests

--- core/tst/micro-packages/run-perf-pure.sh
+++ core/tst/micro-packages/run-perf-pure.sh
@@ -15,11 +15,11 @@ analysis() {
   mv $PRE$m $m
   
   echo "Time $t" >> ../pure-time
-  #loopNumber=100
-  #for ((i=0; i<=loopNumber; i=i+1))
-  #do
-    (time npm test) 2>> ../pure-time
-  #done
+  loopNumber=100
+  for ((i=0; i<=loopNumber; i=i+1))
+  do
+    (time npm test) > /dev/null 2>> ../pure-time
+  done
   
   mv $m $PRE$m
   mv tempFolder/$m $m
