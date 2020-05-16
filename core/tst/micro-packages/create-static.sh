d69a94b1852a14c72854294ae5e7a19f95b849e4
GNtousakis <greco1996@gmail.com>
2020-05-16 16:21:20 +0300

    Change dot to

--- core/tst/micro-packages/create-static.sh
+++ core/tst/micro-packages/create-static.sh
@@ -7,10 +7,10 @@ analysis() {
   t="$(echo $1 | sed 's;/;;' | sed 's;$;:;')"
   m=$(cat package.json | jq .main | tr -d '"')
   
-  mir-sa  $PRE$m > ../staticFiles/static$t.json
-  mir-sa -b . > ../staticFiles/staticbase$t.json
-  mir-sa -f . > ../staticFiles/staticfield$t.json
-  mir-sa -bf . > ../staticFiles/staticboth$t.json
+  mir-sa $PRE$m > ../staticFiles/static$t.json
+  mir-sa -b $PRE$m > ../staticFiles/staticbase$t.json
+  mir-sa -f $PRE$m > ../staticFiles/staticfield$t.json
+  mir-sa -bf $PRE$m > ../staticFiles/staticboth$t.json
   
   cd ..
 }
