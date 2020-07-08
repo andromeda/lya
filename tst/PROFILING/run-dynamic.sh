8270f4d94079707ea301d6ed78b03b657bda93d0
GNtousakis <greco1996@gmail.com>
2020-07-08 12:31:54 +0300

    Add skeleton of PROFILING tests

---
+++ tst/PROFILING/run-dynamic.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -o pipefail
+set -e
+
+analysis() {
+  # Dynamic analysis comes from a different segment so that
+  # static analysis does not get confused
+  cat ../prologue-profiling.lya ../epilogue.lya > generated.test
+  # Replace node with cat to see the generated script
+  node generated.test > /dev/null
+  # run main and compare results with static
+  # node -e 'require("assert").deepStrictEqual(require("./dynamic.json"), require("./correct.json"));'
+  #node -e 'var eq = require("lodash.isequal"); var c = require("./correct.json"); var d = require("./dynamic.json"); if (!eq(c, d)) { console.log(require("json-diff").diffString(c, d)); process.exit(-1); }' | nl
+}
+
+if [ "$#" -eq 1 ]; then
+  cd $1
+  analysis
+  cd ..
+else
+  for d in t?/; do
+    cd $d;
+    analysis
+    cd ..
+  done
+fi
+
---
+++ tst/PROFILING/t0/m2.js
@@ -0,0 +1,1 @@
+console.log('Pizza is life');
