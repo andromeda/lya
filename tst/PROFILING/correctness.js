81515179e955ab872683c3b39bbeeafab19ff7fb
GNtousakis <greco1996@gmail.com>
2020-07-08 20:18:58 +0300

    Update on t1

--- tst/PROFILING/correctness.js
+++ tst/PROFILING/correctness.js
@@ -2,8 +2,13 @@ const baseTruth = require("./correct.json");
 const analysisResult = require("./dynamic.json"); 
 const jsonDiff = require("json-diff");
 
+// We check if the base and result has the same stracture
+const stracture = (a, b) => {
+  console.log(jsonDiff.diffString(a, b, null, {'keysOnly':true}));
+};
+
 // If the tests are too quick we just compare the keys
 if (JSON.stringify(baseTruth).includes('true')) {
-  console.log(jsonDiff.diffString(baseTruth, analysisResult, null, {'keysOnly':true}));
+  stracture(baseTruth, analysisResult);
 }
 
