2d8b4ed9aab54ab92cef5158d7da11c56c5c6fe0
GNtousakis <greco1996@gmail.com>
2020-08-10 21:04:28 +0300

    Add lodash, underscore cases

--- tst/libq/test_case/underscore/main.js
+++
@@ -1,8 +0,0 @@
-const _ = require('underscore');
-const { execSync } = require('child_process');
-
-for (const name in _) {
-  const command = 'cd test; grep ' + name + '.' ;  
-  const result = execSync(command).toString();
-}
-
