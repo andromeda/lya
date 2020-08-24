1b1da4431b415049e9a56b11d10d575873bf4e9f
GNtousakis <greco1996@gmail.com>
2020-08-24 20:01:48 +0300

    Add some tutorial examples

---
+++ doc/tutorial/analyses/imports/m1.js
@@ -0,0 +1,9 @@
+const pineapple = -100;
+const tommato = 10;
+const mozzarella = 10;
+
+const pizza = mozzarella + tommato + pineapple;
+
+const fs = require('fs');
+const path = require('path');
+require('./m2.js');
