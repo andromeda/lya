7108b897fd9d3e5867a937f82dbcd43fbc01c181
GNtousakis <greco1996@gmail.com>
2020-08-26 12:06:17 +0300

    Simplify call-freq

--- src/analysis/performance/call-freq.js
+++ src/analysis/performance/call-freq.js
@@ -1,14 +1,6 @@
 let env;
-const pattern = /require[(](.*)[)]/;
 const fs = require('fs');
 
-// Change the time parameters
-const convert = (hrtime) => {
-  const nanos = (hrtime[0] * 1e9) + hrtime[1];
-  const millis = nanos / 1e6;
-  const secs = nanos / 1e9;
-  return {secs: secs, millis: millis, nanos: nanos};
-};
 // @storedCalls it is a table that contains all the analysis data
 // @truename the name of the current function, object etc that we want to add to
 // the table
@@ -22,49 +14,10 @@ const updateAnalysisData = (storedCalls, truename) => {
   }
 };
 
-// Analyses provided by LYA.
-// onRead <~ is called before every object is read
-const onRead = (info) => {
-  if (info.nameToStore !== 'global') {
-    if (pattern.test(info.nameToStore)) {
-      updateAnalysisData(env.analysisResult[info.currentModule],
-          info.nameToStore.match(pattern)[0]);
-    } else {
-      updateAnalysisData(env.analysisResult[info.currentModule],
-          info.nameToStore.split('.')[0]);
-    }
-    updateAnalysisData(env.analysisResult[info.currentModule],
-        info.nameToStore);
-  }
-};
-
-// onWrite <~ is called before every write of an object
-const onWrite = (info) => {
-  if (info.parentName) {
-    updateAnalysisData(env.analysisResult[info.currentModule], info.parentName);
-  }
-  updateAnalysisData(env.analysisResult[info.currentModule], info.nameToStore);
-};
-
 // onCallPre <~ is called before the execution of a function
 const onCallPre = (info) => {
-  if (info.typeClass === 'module-locals') {
-    updateAnalysisData(env.analysisResult[info.currentModule],
-        'require');
-    updateAnalysisData(env.analysisResult[info.currentModule],
-        info.nameToStore);
-  } else {
-    if (info.typeClass === 'node-globals') {
-      updateAnalysisData(env.analysisResult[info.declareModule],
-          info.nameToStore.split('.')[0]);
-    }
-    updateAnalysisData(env.analysisResult[info.declareModule],
-        info.nameToStore);
-    if (pattern.test(info.nameToStore)) {
-      updateAnalysisData(env.analysisResult[info.currentModule],
-          info.nameToStore.match(pattern)[0]);
-    }
-  }
+  updateAnalysisData(env.analysisResult[info.currentModule],
+      info.nameToStore);
 };
 
 // onExit (toSave == place to save the result) --maybe make it module-local?
@@ -74,23 +27,19 @@ const onExit = (intersection, candidateModule) => {
     updateAnalysisData(env.analysisResult[currentName],
         name);
   }
-  if (env.conf.reportTime) {
-    const timerEnd = process.hrtime(env.conf.timerStart);
-    const timeMillis = convert(timerEnd).millis;
-    console.log(timeMillis, 'Time');
-  }
   if (env.conf.SAVE_RESULTS) {
     fs.writeFileSync(env.conf.SAVE_RESULTS,
         JSON.stringify(env.analysisResult, null, 2), 'utf-8');
+  }
+  if (env.conf.print) {
+    console.log(JSON.stringify(env.analysisResult, null, 2), 'utf-8');
   }
 };
 
 module.exports = (e) => {
   env = e;
   return {
-    onRead: onRead,
     onCallPre: onCallPre,
-    onWrite: onWrite,
     onExit: onExit,
   };
 };
