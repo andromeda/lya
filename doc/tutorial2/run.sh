01a41635fe7c69bd7f81ab2852867fbe45bdc192
Nikos Vasilakis <nikos@vasilak.is>
2021-03-17 02:38:56 -0400

    Update run.sh

--- doc/tutorial2/run.sh
+++ doc/tutorial2/run.sh
@@ -14,46 +14,33 @@ msg "Starting lya demo2"
 sleep 4
 
 echo -e "\n"
-msg "Example of a bottleneck module"
-sleep 2
-cd bottleneck
-msg "We first run the program without any analysis"
-node example.js
-msg "We can see that the program run quite slowly"
-sleep 2
-msg "We run it again with a performance analysis to detect the slow parts of the
-program"
-../../../lya.js example.js -p -a call-times
-msg "We can see that the slow function is require('crypto').pbkdf2Sync"
-cd ..
-sleep 2
-
-echo -e "\n"
-msg "Example of a payload offload to a remote web server that executes a eval
-command"
+msg "Example of a payload offload to a remote web server that executes a eval command"
 sleep 2
 cd eval
-rm personal.data -rf
-msg "We check that are is not a personal.data file on directory"
-ls 
+rm personal.data
+msg "We first confirm that are is no personal.data file in this directory"
+ls -l *.data
 sleep 2 
 msg "We run the attack"
-msg "Run 'node http-server.js' on another terminal window"
-read -p "Press any key to continue... " -n1 -s
+msg "We first launch the HTTP server"
+node http-server.js &
+msg "We then send a malicious payload"
 node attack.js
-msg "We check again that a personal.data has been created in directory"
-ls 
+msg "We confirm that a personal.data has been created"
+ls -l *.data
 sleep 2
 msg "The payload created a personal.data file on the server"
-msg "We rerun the attack in order to detect it with lya"
+msg "We now run the HTTP server with lya"
 sleep 2
-msg "Run '../../../lya.js http-server.js -p --prop-exclude eval' on another terminal window"
-read -p "Press any key to continue... " -n1 -s
+# (Bug? why exclude eval from tracing?)
+../../../lya.js http-server.js -p --prop-exclude eval &
+msg "We then send a malicious payload"
 node attack.js
 echo -e "\n"
 msg "The eval attack should be printed"
 cd ..
 sleep 2
+# (Bug?: why not show accesses in the context of the library?)
 
 echo -e "\n"
 msg "We try to detect a module turned malicious"
@@ -63,16 +50,30 @@ rm node_modules -rf
 msg "We install version 1.0.0 of @andromeda/calc"
 npm install @andromeda/calc@1.0.0
 node main.js
-msg "This version is pure"
+msg "This version is not malicious and works as expected"
 sleep 2
-msg "We install version 2.0.0 of @andromeda/calc"
+msg "We now install version 2.0.0 of @andromeda/calc"
 npm install @andromeda/calc@2.0.0
 ../../../lya.js main.js -p --prop-exclude eval
 msg "This is a malicious version"
-msg "It reads personal.info file and writes your data at pass.info"
+msg "It reads a personal.info file and writes to pass.info"
 ls
 sleep 2
 
+
+echo -e "\n"
+msg "Example of a bottleneck module"
+sleep 2
+cd bottleneck
+msg "We first run the program without any analysis"
+node example.js
+msg "The program has a performance bottleneck"
+sleep 2
+msg "We run it again with a simple performance analysis to extract call times"
+../../../lya.js example.js -p -a call-times
+msg "We can see that pbkdf2Sync is the slowest function call"
+cd ..
+sleep 2
 
 echo -e "\n\n"
 msg "End of demo2!"
