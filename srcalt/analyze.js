b80235bf1a4cd55637f92e0974ab1c96c2953b6c
Sage Gerard <sage@sagegerard.com>
2021-03-23 17:03:31 -0400

    Revert "Use runInThisContext()"
    This reverts commit e77d1f9b1f243f597235a1888438f44ce5444c9b.

--- srcalt/analyze.js
+++ srcalt/analyze.js
@@ -12,78 +12,75 @@ const fs = require('fs');
 const vm = require('vm');
 const { coerceString } = require('./string.js');
 const { assert, test } = require('./test.js');
-const { noop } = require('./functions.js');
-const { callWithOwnValues } = require('./container-type.js');
-
-
-// A pattern for obtaining the global object regardless of the JS
-// runtime used.
-const universalGlobal = new Function('return this')();
 
 // Called for its effect
 function analyze(env) {
-    const { entry } = env;
+    const {
+        entry,
+        context: contextVariant,
+        conf,
+    } = env || {};
+
+    const {
+        context: contextConfig,
+        vm: vmConfig,
+    } = conf || {};
+
     const code = coerceString(entry, { allowFileRead: true });
 
-    try {
-        // runInThisContext would not introduce behavioral difference
-        // from runInContext, but paper theses prefer to reason about
-        // a context that already exists for built-in references on
-        // the C++ side of things.
-        env.value = vm.runInThisContext(code, env);
-    } catch (e) {
-        env.value = e;
-    }
+    env.context = vm.isContext(contextVariant)
+        ? contextVariant
+        : vm.createContext(contextVariant, contextConfig);
+
+    env.value = vm.runInContext(code, env.context, vmConfig);
 
     return env;
 }
 
 // Simple use
 test(() => {
-    const g = universalGlobal;
-    const exit = v => v + 10;
+    const options = {
+        entry: 'x = process.exit(x)',
+        context: {
+            x: 0,
+            process: {
+                exit: (v) => v + 10,
+            },
+        },
+    };
 
-    callWithOwnValues(g, { x: 8, process: Object.assign({}, process, { exit }) }, () => {
-        const options = {
-            entry: 'x = process.exit(x)',
-        };
+    const ref = analyze(options);
 
-        const ref = analyze(options);
+    assert(ref === options, 'Operate on the input argument');
+    assert(ref.context.x === 10, 'JS affects context object');
+    assert(ref.value === 10, 'Evaluate to last statement or expression.');
 
-        assert(typeof g === 'object', 'Capture global');
-        assert(ref === options, 'Operate on the input argument');
-        assert(g.x === 18, 'JS affects context object');
-        assert(ref.value === 18, 'Evaluate to last statement or expression.');
+    analyze(options);
 
-        analyze(options);
-
-        assert(g.x === 28 && g.x === ref.value,
-               'Reuse context');
-    });
+    assert(ref.context.x === 20 && ref.context.x === ref.value,
+           'Reuse context');
 });
 
 
 // Recursive eval example
 test(() => {
-    const g = universalGlobal;
-
     const options = {
         // Expression ultimately means: 2 * 8 + -1 => 14
         entry: 'x = eval("2 * eval(`8 + eval(\'x\')`)")',
+        context: {
+            eval: (entry) => {
+                // Base case is to reference only the global variable.
+                if (entry === 'x') {
+                    return -1;
+                } else {
+                    return analyze(Object.assign({}, options, { entry })).value;
+                }
+            },
+        },
     };
 
-    const eval = function eval(entry) {
-        // Base case is to reference only the global variable.
-        if (entry === 'x') {
-            return -1;
-        } else {
-            return analyze(Object.assign({}, options, { entry })).value;
-        }
-    };
+    analyze(options);
 
-    callWithOwnValues(g, { x: -1, eval }, () => {
-        analyze(options);
-        assert(g.x === 14 && g.x === options.value,
-               'Recursively analyze via eval()')
-    });
+    assert(options.context.x === 14 && options.context.x === options.value,
+           'Recursively analyze via eval()');
 });
