89d83e9330b1180f0e7842748b5df3fff8822703
Sage Gerard <sage@sagegerard.com>
2021-04-01 14:26:52 -0400

    Do not use outer context eval for recursive Lya use

--- tst/eval-analysis.js
+++ tst/eval-analysis.js
@@ -2,8 +2,6 @@ module.exports = (lya) => {
   let env = lya.createLyaState({
     hooks: {
       onCallPre: ({ currentModule, target, argumentsList, nameToStore }) => {
-        if (target === eval) lya.analyze(Object.assign({}, env, {entry: argumentsList[0]}))
-
         const storedCalls = env.results[currentModule] = env.results[currentModule] || {};
         storedCalls[nameToStore] = (storedCalls[nameToStore] || 0) + 1;
       },
