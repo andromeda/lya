bbdcd16de7a38e0e3091720f33ff00b0c114eb45
Sage Gerard <sage@sagegerard.com>
2021-04-15 17:19:05 -0400

    Remove dead withHandlers function

--- src/control.js
+++ src/control.js
@@ -3,7 +3,6 @@
 module.exports = {
   failTo,
   raise,
-  withHandlers,
   withCatch,
 };
 
@@ -26,44 +25,10 @@ function withCatch(c, f) {
   }
 }
 
-function withHandlers(specs, f) {
-  try {
-    return f();
-  } catch (e) {
-    const matching = specs.find((a) => a[0](e));
-
-    if (matching) {
-      return matching[1](e);
-    } else {
-      throw e;
-    }
-  }
-}
-
-
 test(() => {
   assert(failTo(1, () => raise(2)) === 1,
       'Represent any thrown value as another value');
 
   assert(withCatch((e) => e === 5, () => raise(5)),
       'Catch a value, then control the apparant return value of another function');
-
-  const handlers = [
-    [
-      (e) => e === 1,
-      (e) => e + 1,
-    ],
-    [
-      (e) => e === 'a',
-      (e) => e + 'bc',
-    ],
-  ];
-
-  assert(withHandlers(handlers, () => raise(1)) === 2 &&
-           withHandlers(handlers, () => raise('a')) === 'abc',
-  'Match thrown values against handlers');
-
-  assert(withHandlers([[() => true, () => 'caught']],
-      () => withHandlers(handlers, () => raise())) === 'caught',
-  'Escalate uncaught values');
 });
