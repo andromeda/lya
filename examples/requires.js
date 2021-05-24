3a6a1becb9e1fa10628f2b0a886977af937f2417
Sage Gerard <sage@sagegerard.com>
2021-05-24 15:07:08 -0400

    Add examples/requires.js

---
+++ examples/requires.js
@@ -0,0 +1,26 @@
+module.exports = function importAnalysis(argv, lya) {
+  const results = {};
+
+  return {
+    onReady: function onReady () {
+      return require(lya.findEntryModule(require, argv[0]));
+    },
+
+    onApply: function onApply(original, info) {
+      const result = original();
+
+      var I = info.instrumentation;
+
+      // "Did a module call its own require function?"
+      if (I.require === info.target) {
+        results[I.module.id] = results[I.module.id] || [];
+        results[I.module.id].push(info.args[0]);
+      }
+      
+      return result;
+    },
+    afterAnalysis: function afterAnalysis() {
+      return JSON.stringify(results, null, 2);
+    },
+  };
+};
